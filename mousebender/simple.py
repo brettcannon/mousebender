f8df4e72111424ac453fe209c720167d97cfdde5
Derek Keeler <dekeeler@microsoft.com>
2020-01-16 21:26:04 -0800

    Complete implementing simple index parser
    - Add .vscode to .gitignore
    - Add vscode 'recommended settings'
    - Normalize project name in index urls (as per spec)
    - Add tests for normalization
    - Add tests for return values from simple parser

--- mousebender/simple.py
+++ mousebender/simple.py
@@ -2,14 +2,25 @@
 
 import dataclasses
 import html.parser
+import re
+
+# Regex to normalize project names for packages (see pep-0503).
+_NORMALIZE_RE = re.compile(r"[-_.]+")
 
 
-class SimpleIndexHTMLParser(html.parser.HTMLParser):
+def normalize_project_url(name):
+    """Returns a normalized project url as per PEP 503."""
+    name = name.rstrip("/")
+    name_prefix, sep, project = name.rpartition("/")
+    normalized_project = _NORMALIZE_RE.sub("-", project).lower()
+    return "".join([name_prefix, sep, normalized_project, "/"])
 
+
+class _SimpleIndexHTMLParser(html.parser.HTMLParser):
     def __init__(self):
         super().__init__()
         self._parsing_anchor = False
-        self._uri = None
+        self._url = None
         self._name = None
         self.mapping = {}
 
@@ -17,25 +28,29 @@ class SimpleIndexHTMLParser(html.parser.HTMLParser):
         if tag != "a":
             return
         self._parsing_anchor = True
-        self._uri = dict(attrs)["href"]
+        self._url = dict(attrs).get("href")
 
     def handle_endtag(self, tag):
         if tag != "a":
             return
-        self.mapping[self._name] = self._uri
-        self._name = self._uri = None
+        elif not self._name or not self._url:
+            return
+        else:
+            self.mapping[self._name] = normalize_project_url(self._url)
+
+        self._name = self._url = None
         self._parsing_anchor = False
 
     def handle_data(self, data):
         if self._parsing_anchor:
             self._name = data
 
+
 # input: bytes of a simple index file
-# output: `package-index`: dict containing key:`project-name` and val:`project-uri`
-def parse_package_index(index_html):
+# output: `package-index`: dict containing key:`project-name` and val:`project-url`
+def parse_projects_index(index_html):
     # parse this index_html expecting n package names. Each package has associated to it:
     # - A url (to the project-index)
-    
-    parser = SimpleIndexHTMLParser()
+    parser = _SimpleIndexHTMLParser()
     parser.feed(index_html)
     return parser.mapping
