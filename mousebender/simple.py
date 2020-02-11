7a27210f8123cbc8ebe72f67f209f8322b3dcfe7
Derek Keeler <dekeeler@microsoft.com>
2020-02-11 12:12:55 -0800

    Add gpg-sig-data tests and functionality.
    - Correct logic in setting the tri-value flag

--- mousebender/simple.py
+++ mousebender/simple.py
@@ -81,7 +81,6 @@ class ProjectFileInfo:
         hash_algo, _, hash_val = hash_info.partition("=")
         if hash_algo and hash_val:
             file_details["hash"] = hash_algo, hash_val
-        file_details["gpg_sig"] = None
 
         return cls(**file_details)
 
@@ -99,8 +98,11 @@ class _ProjectFileHTMLParser(html.parser.HTMLParser):
         self._parsing_anchor = True
         attrs = dict(attrs_list)
         self._file["url"] = attrs.get("href")
-        if gpg_sig := attrs.get("data-gpg-sig"):
-            self._file["gpg_sig"] = gpg_sig == "true"
+
+        if gpg_sig := attrs.get("data-gpg-sig", None):
+            gpg_sig = gpg_sig == "true"
+        self._file["gpg_sig"] = gpg_sig
+
         self._file["requires_python"] = attrs.get("data-requires-python")
 
     def handle_endtag(self, tag):
