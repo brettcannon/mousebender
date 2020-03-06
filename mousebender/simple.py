7f517519e954c0797cc2e88bbe62a286f259dcec
Brett Cannon <54418+brettcannon@users.noreply.github.com>
2020-03-05 18:49:05 -0800

    Touch up code (#6)

--- mousebender/simple.py
+++ mousebender/simple.py
@@ -1,4 +1,4 @@
-"""Simple index parsing module"""
+"""Parsing for PEP 503 -- Simple Repository API."""
 from __future__ import annotations
 
 import dataclasses
@@ -6,19 +6,47 @@ import html.parser
 import re
 from typing import Optional, Tuple
 
-# Regex to normalize project names for packages (see pep-0503).
+
 _NORMALIZE_RE = re.compile(r"[-_.]+")
 
 
-def normalize_project_url(name):
-    """Returns a normalized project url as per PEP 503."""
-    name = name.rstrip("/")
-    name_prefix, sep, project = name.rpartition("/")
-    normalized_project = _NORMALIZE_RE.sub("-", project).lower()
-    return "".join([name_prefix, sep, normalized_project, "/"])
+def create_project_url(base_url, project_name):
+    """Construct the project URL for a repository following PEP 503."""
+    if not base_url.endswith("/"):
+        base_url += "/"
+    # https://www.python.org/dev/peps/pep-0503/#normalized-names
+    normalized_project_name = _NORMALIZE_RE.sub("-", project_name).lower()
+    # PEP 503:
+    # The format of this URL is /<project>/ where the <project> is replaced by
+    # the normalized name for that project, so a project named "HolyGrail" would
+    # have a URL like /holygrail/.
+    #
+    # All URLs which respond with an HTML5 page MUST end with a / and the
+    # repository SHOULD redirect the URLs without a / to add a / to the end.
+    return "".join([base_url, normalized_project_name, "/"])
+
+
+def _normalize_project_url(url):
+    """Normalizes a project URL found in a repository index.
+
+    If a repository is fully-compliant with PEP 503 this will be a no-op.
+    If a repository is reasonably compliant with PEP 503 then the resulting URL
+    will be usable.
+
+    """
+    url_no_trailing_slash = url.rstrip("/")  # Explicitly added back later.
+    base_url, _, project_name = url_no_trailing_slash.rpartition("/")
+    return create_project_url(base_url, project_name)
 
 
 class _SimpleIndexHTMLParser(html.parser.HTMLParser):
+
+    """Parse the HTML of a repository index page."""
+
+    # PEP 503:
+    # Within a repository, the root URL (/) MUST be a valid HTML5 page with a
+    # single anchor element per project in the repository.
+
     def __init__(self):
         super().__init__()
         self._parsing_anchor = False
@@ -27,6 +55,9 @@ class _SimpleIndexHTMLParser(html.parser.HTMLParser):
         self.mapping = {}
 
     def handle_starttag(self, tag, attrs):
+        # PEP 503:
+        # There may be any other HTML elements on the API pages as long as the
+        # required anchor elements exist.
         if tag != "a":
             return
         self._parsing_anchor = True
@@ -36,7 +67,7 @@ class _SimpleIndexHTMLParser(html.parser.HTMLParser):
         if tag != "a":
             return
         elif self._name and self._url:
-            self.mapping[self._name] = normalize_project_url(self._url)
+            self.mapping[self._name] = _normalize_project_url(self._url)
 
         self._name = self._url = None
         self._parsing_anchor = False
@@ -46,13 +77,14 @@ class _SimpleIndexHTMLParser(html.parser.HTMLParser):
             self._name = data
 
 
-# input: bytes of a simple index file
-# output: `package-index`: dict containing key:`project-name` and val:`project-url`
-def parse_projects_index(index_html):
+def parse_repo_index(index_html):
+    """Parse the HTML for a repository index page."""
     parser = _SimpleIndexHTMLParser()
     parser.feed(index_html)
     return parser.mapping
 
+
+# XXX Draft code for archive links parsing =====================================
 
 # Data to store for the simple project index:
 # - filename
@@ -63,11 +95,11 @@ def parse_projects_index(index_html):
 
 
 @dataclasses.dataclass
-class ProjectFileInfo:
+class ArchiveLink:
     filename: str
     url: str
     hash: Optional[Tuple[str, str]]
-    requires_python: Optional[str]
+    requires_python: Optional[str]  # XXX packaging.specifiers.SpecifierSet?
     gpg_sig: Optional[bool]
 
     @classmethod
@@ -116,8 +148,7 @@ class _ProjectFileHTMLParser(html.parser.HTMLParser):
             self._file["filename"] = data
 
 
-def parse_file_index(index_html):
-    """Brett will never rename this amazing function name. --Derek"""
+def parse_archive_links(index_html):
     # for each simple file anchor set, consisting of
     # href, cdata, and attributes, construct a ProjectFileInfo
     # and add it to the set of files contained in a version member
--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,66 +1,78 @@
 """Tests for mousebender.simple."""
 import importlib.resources
 
+import pytest
+
 from mousebender import simple
 
 from . import data
 
 
-def test_simple_index_basic():
-    index_html = importlib.resources.read_text(data, "simple.index.html")
-    index = simple.parse_projects_index(index_html)
-    assert "numpy" in index
-    assert index["numpy"] == "/simple/numpy/"
-    assert len(index) == 212_862
+class TestProjectURLConstruction:
+
+    """Tests for mousebender.simple.create_project_url()."""
+
+    @pytest.mark.parametrize("base_url", ["/simple/", "/simple"])
+    def test_url_joining(self, base_url):
+        url = simple.create_project_url(base_url, "hello")
+        assert url == "/simple/hello/"
+
+    def test_project_name_lowercased(self):
+        url = simple.create_project_url("/", "THEPROJECTNAME")
+        assert url == "/theprojectname/"
+
+    def test_project_name_normalized(self):
+        normal_url = simple.create_project_url("/", "the_project.name.-_.-_here")
+        assert normal_url == "/the-project-name-here/"
+
+    def test_only_project_name_in_url_normalized(self):
+        url = simple.create_project_url(
+            "https://terribly_awesome.com/So/Simple/", "THE_project.name.-_.-_here"
+        )
+        assert url == "https://terribly_awesome.com/So/Simple/the-project-name-here/"
 
 
-def test_simple_index_no_cdata():
-    index_html = (
-        "<html><head></head><body><a href='https://no.url/here'></a></body></html>"
-    )
-    index = simple.parse_projects_index(index_html)
-    assert not index
+class TestRepoIndexParsing:
 
+    """Tests for mousebender.simple.parse_repo_index()."""
 
-def test_simple_index_no_href():
-    index_html = "<html><head></head><body><a>my-cdata-package</a></body></html>"
-    index = simple.parse_projects_index(index_html)
-    assert not index
+    def test_baseline(self):
+        index_html = importlib.resources.read_text(data, "simple.index.html")
+        index = simple.parse_repo_index(index_html)
+        assert "numpy" in index
+        assert index["numpy"] == "/simple/numpy/"
+        assert len(index) == 212_862
 
+    def test_no_cdata(self):
+        index_html = (
+            "<html><head></head><body><a href='https://no.url/here'></a></body></html>"
+        )
+        index = simple.parse_repo_index(index_html)
+        assert not index
 
-def test_normalize_package_project_url_lowercase():
-    project_url = "/THEPROJECTNAME/"
-    normal_url = simple.normalize_project_url(project_url)
-    assert normal_url == project_url.lower()
+    def test_no_href(self):
+        index_html = "<html><head></head><body><a>my-cdata-package</a></body></html>"
+        index = simple.parse_repo_index(index_html)
+        assert not index
 
+    def test_project_url_normalization_complete(self):
+        index_html = """
+            <html>
+                <body>
+                    <a href="/project/PACKAGE-NAME">package-name</a>
+                </body>
+            </html>
+        """
+        index = simple.parse_repo_index(index_html)
+        assert index["package-name"] == "/project/package-name/"
 
-def test_normalize_package_project_url_trailing_slash():
-    project_url = "/the-project-name"
-    normal_url = simple.normalize_project_url(project_url)
-    assert normal_url == f"{project_url}/"
-
-
-def test_normalize_package_project_url_substitutions():
-    project_url = "/the_project.name.-_.-_here/"
-    expected_url = "/the-project-name-here/"
-    normal_url = simple.normalize_project_url(project_url)
-    assert normal_url == expected_url
-
-
-def test_normalize_package_project_url_only():
-    project_url = "https://terribly_awesome.com/So/Simple/THE_project.name.-_.-_here"
-    expected_url = "https://terribly_awesome.com/So/Simple/the-project-name-here/"
-    normal_url = simple.normalize_project_url(project_url)
-    assert normal_url == expected_url
-
-
-def test_ensure_normalization_called():
-    index_html = """
-        <html>
-            <body>
-                <a href="/project/PACKAGE-NAME">package-name</a>
-            </body>
-        </html>
-    """
-    index = simple.parse_projects_index(index_html)
-    assert index["package-name"] == "/project/package-name/"
+    def test_project_name_not_normalized(self):
+        index_html = """
+            <html>
+                <body>
+                    <a href="/project/package-name">PACKAGE-NAME</a>
+                </body>
+            </html>
+        """
+        index = simple.parse_repo_index(index_html)
+        assert index["PACKAGE-NAME"] == "/project/package-name/"
