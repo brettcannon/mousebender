7fad4bc36f8b250e9c21507a0ad92a97c8538a69
Brett Cannon <brett@python.org>
2022-11-11 20:52:12 -0800

    Get `TypedDict` from `typing_extensions`

--- mousebender/simple.py
+++ mousebender/simple.py
@@ -3,13 +3,13 @@ from __future__ import annotations
 import html
 import html.parser
 import urllib.parse
-from typing import Any, Dict, List, TypedDict, Union
+from typing import Any, Dict, List, Union
 
 import packaging.specifiers
 import packaging.utils
 
 # Python 3.8+ only.
-from typing_extensions import Literal
+from typing_extensions import Literal, TypedDict
 
 
 def create_project_url(base_url: str, project_name: str) -> str:
