8ea8ea71fff3179bdc71f21004af6c442973d052
Brett Cannon <brett@python.org>
2022-11-12 17:54:35 -0800

    Use `noxfile.py` for formatting

--- noxfile.py
+++ noxfile.py
@@ -3,23 +3,37 @@ import nox
 python_versions = ["3.7", "3.8", "3.9", "3.10", "3.11"]
 
 
-def run_tests(session, extras=[]):
-    session.install("-e", ".[test]")
-    session.run("pytest", *extras)
-
-
 @nox.session(python=python_versions)
-def test(session):
-    run_tests(session)
+def test(session, coverage=False):
+    """Run the test suite."""
+    session.run("pytest", *(["--cov"] if coverage else []))
 
 
 @nox.session(python=python_versions)
 def coverage(session):
-    run_tests(session, ["--cov"])
+    """Run the test suite under coverage."""
+    test(session, coverage=True)
 
 
 @nox.session
-def type_check(session):
+def check_types(session):
+    """Type check."""
     session.install("-e", ".")
     session.install("mypy")
     session.run("mypy", "mousebender")
+
+
+@nox.session
+def format(session, check=False):
+    """Format the code."""
+    for tool in ("black", "isort"):
+        session.install(tool)
+        args = ["--check"] if check else []
+        args.append(".")
+        session.run(tool, *args)
+
+
+@nox.session
+def check_format(session):
+    """Check that the code is properly formatted."""
+    format(session, check=True)
