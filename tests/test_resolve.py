5e302f098d4b05c363de66b34b90457694d0be45
Brett Cannon <brett@python.org>
2024-02-05 17:53:32 -0800

    Test for requirement under different extra names for `get_dependencies()`

--- tests/test_resolve.py
+++ tests/test_resolve.py
@@ -1023,6 +1023,39 @@ class TestGetDependencies:
         ]
         assert dependencies == expected
 
+    def test_requirement_listed_under_different_extras(self):
+        details: simple.ProjectFileDetails_1_0 = {
+            "filename": "Spam-1.2.3-456-py3-none-any.whl",
+            "url": "spam.whl",
+            "hashes": {},
+        }
+        metadata = packaging.metadata.Metadata.from_raw(
+            typing.cast(
+                packaging.metadata.RawMetadata,
+                {
+                    "metadata_version": "2.3",
+                    "name": "Spam",
+                    "version": "1.2.3",
+                    "requires_dist": [
+                        "bacon; extra=='bonus'",
+                        "bacon; extra=='unimportant'",
+                    ],
+                },
+            )
+        )
+        file = resolve.WheelFile(details)
+        file.metadata = metadata
+        candidate = resolve.Candidate(identifier("spam", {"bonus"}), file)
+        provider = LocalWheelProvider()
+
+        dependencies = provider.get_dependencies(candidate)
+        expected = [
+            resolve.Requirement(packaging.requirements.Requirement("spam==1.2.3")),
+            resolve.Requirement(packaging.requirements.Requirement("bacon")),
+        ]
+        assert dependencies == expected
+
+
 
 
     # XXX prefer newest release
