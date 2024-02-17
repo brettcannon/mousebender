0cf43e7fbb6dffcf784274eec43cb7646e2767c7
Brett Cannon <brett@python.org>
2024-02-16 17:20:42 -0800

    Initial stab at a locker

---
+++ __main__.py
@@ -0,0 +1,118 @@
+# ruff: noqa: ANN001, ANN201, D100, D103
+import argparse
+import sys
+
+import httpx
+import packaging.metadata
+import packaging.requirements
+import packaging.utils
+import resolvelib.resolvers
+import tomllib
+import trio
+
+import mousebender.lock
+import mousebender.resolve
+import mousebender.simple
+
+
+async def get_metadata_for_file(client, file):
+    """Get the METADATA file for a wheel."""
+    url = file.details["url"] + ".metadata"
+    response = await client.get(url)
+    raw_data = response.content
+    if isinstance(file.details.get("core-metadata", True), dict):
+        # XXX If core-metadata is a dict, verify the hash of the data
+        pass
+    metadata = packaging.metadata.Metadata.from_email(raw_data, validate=False)
+    file.metadata = metadata
+
+
+async def get_metadata(files):
+    """Get the METADATA file for a wheel."""
+    async with httpx.AsyncClient() as client:
+        async with trio.open_nursery() as nursery:
+            for file in files:
+                nursery.start_soon(get_metadata_for_file, client, file)  # type: ignore[arg-type]
+
+
+class PyPIProvider(mousebender.resolve.WheelProvider):
+    """A provider for wheels from PyPI."""
+
+    def available_files(self, name):
+        """Get the available wheels for a distribution."""
+        project_url = mousebender.simple.create_project_url(
+            "https://pypi.org/simple", name
+        )
+        response = httpx.get(
+            project_url,
+            follow_redirects=True,
+            headers={"Accept": mousebender.simple.ACCEPT_SUPPORTED},
+        )
+
+        project_details = mousebender.simple.parse_project_details(
+            response.text, response.headers["Content-Type"], name
+        )
+
+        nothing_yanked = filter(
+            lambda f: not f.get("yanked", False), project_details["files"]
+        )
+
+        wheels = filter(lambda f: f["filename"].endswith(".whl"), nothing_yanked)
+        has_metadata = filter(lambda f: f.get("core-metadata", False), wheels)
+
+        return map(mousebender.resolve.WheelFile, has_metadata)
+
+    def fetch_metadata(self, file_details):
+        """Fetch the METADATA file for a wheel."""
+        trio.run(get_metadata, file_details)
+
+
+def resolve(requirements):
+    provider = PyPIProvider()
+    reporter = resolvelib.BaseReporter()
+    resolver: resolvelib.Resolver = resolvelib.Resolver(provider, reporter)
+    try:
+        resolution = resolver.resolve(requirements)
+    except resolvelib.ResolutionImpossible:
+        print("Resolution (currently) impossible 😢")
+        sys.exit(1)
+
+    return provider, resolution
+
+
+def lock(context):
+    if not (dependencies := context.requirements):
+        with open("pyproject.toml", "rb") as file:
+            pyproject = tomllib.load(file)
+        dependencies = pyproject["project"]["dependencies"]
+
+    requirements = map(
+        mousebender.resolve.Requirement,
+        map(packaging.requirements.Requirement, dependencies),
+    )
+
+    provider, resolution = resolve(requirements)
+
+    lock_contents = mousebender.lock.generate_lock(provider, resolution)
+    lock_file = mousebender.lock.generate_file_contents(
+        context.requirements, [lock_contents]
+    )
+
+    print(lock_file)
+
+
+def main(args=sys.argv[1:]):
+    parser = argparse.ArgumentParser()
+    subcommands = parser.add_subparsers(dest="subcommand")
+    lock_args = subcommands.add_parser("lock", help="Generate a lock file")
+    lock_args.add_argument("requirements", nargs="*")
+
+    context = parser.parse_args(args)
+    dispatch = {"lock": lock}
+    dispatch[context.subcommand](context)
+
+
+if __name__ == "__main__":
+    main()
+    # py . lock numpy mousebender hatchling requests pydantic
+    # trio
