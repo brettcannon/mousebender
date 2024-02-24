22cda1812c884b7828676814e2d8f33ec43d38c7
Brett Cannon <brett@python.org>
2024-02-23 16:43:23 -0800

    Print out what files are being written by `update-examples.sh`

--- update-examples.sh
+++ update-examples.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
-echo "Lock graph example ..."
 GRAPH_TOML=pylock.graph-example.toml
+echo "Lock the graph example in $GRAPH_TOML ..."
 py . lock numpy mousebender hatchling requests pydantic > $GRAPH_TOML
-echo "Generate graph ..."
-py . graph --self-contained $GRAPH_TOML > pylock.graph-example.md
+GRAPH_MD=pylock.graph-example.md
+echo "Generate the graph in $GRAPH_MD ..."
+py . graph --self-contained $GRAPH_TOML > $GRAPH_MD
 
-echo "Initial 'maximize' example for CPython 3.10 manylinux2014 x64 ..."
 COMPATIBILITY_TOML=pylock.compatibility-example.toml
+echo "Start a compatibility example for CPython 3.10 manylinux2014 x64 in $COMPATIBILLITY_TOML ..."
 py . lock --platform cpython3.10-manylinux2014-x64 debugpy > $COMPATIBILITY_TOML
 echo "Add CPython 3.12 Windows x64 lock entry ..."
 py . add-lock-entry --platform cpython3.12-windows-x64 $COMPATIBILITY_TOML > /dev/null
-echo "Add compatibility lock entry ..."
+echo "Add pure Python 3.12 lock entry ..."
 py . add-lock-entry --platform python3.12 $COMPATIBILITY_TOML > /dev/null
+
+echo "Install from $COMPATIBILITY_TOML ..."
+py . install $COMPATIBILITY_TOML
