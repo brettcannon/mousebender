6bc59231265b128080ec228958581acef809147a
Brett Cannon <brett@python.org>
2024-07-25 11:35:06 -0700

    Add example of version shift

--- update-examples.sh
+++ update-examples.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 LOCK_FILE_PATH=pylock.example.toml
 echo "Lock the example in $LOCK_FILE_PATH ..."
-py . lock --platform cpython3.12-manylinux2014-x64 --platform cpython3.12-windows-x64 numpy mousebender hatchling requests pydantic trio > $LOCK_FILE_PATH
+py . lock --platform cpython3.12-manylinux2014-x64 --platform cpython3.12-windows-x64 "numpy<2" mousebender hatchling requests pydantic trio > $LOCK_FILE_PATH
 
 echo "Install from $LOCK_FILE_PATH ..."
 py . install $LOCK_FILE_PATH
